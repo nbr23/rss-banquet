d1c63ebc0e402a1ca1a0ad526b77105df97d2d7d
Maxence <max@23.tf>
2023-08-16 22:45:16 -0400

    main.go

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/nbr23/atomic-banquet
+
+go 1.20
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/gorilla/feeds v1.1.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func printHelp() {
+	fmt.Println("Usage: atomic-banquet [-h] [-c config]")
+	flag.PrintDefaults()
+	fmt.Println("\nModules available:")
+
+	sortedModules := make([]string, 0, len(Modules))
+	for key := range Modules {
+		sortedModules = append(sortedModules, key)
+	}
+	sort.Strings(sortedModules)
+
+	for _, module := range sortedModules {
+		fmt.Printf("  - %s\n%s\n", module, Modules[module]().Help())
+	}
+}
+
+func main() {
+	var (
+		showHelp   bool
+		configPath string
+	)
+
+	flag.BoolVar(&showHelp, "h", false, "Show help message")
+	flag.StringVar(&configPath, "c", "./config.yaml", "Path to configuration file")
+	flag.Parse()
+
+	if showHelp {
+		printHelp()
+		return
+	}
+
+	config, err := getFeedsFromConfig(configPath)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	for _, feed := range config.Feeds {
+		module := Modules[feed.Module]()
+		feed, err := module.Parse(feed.Options)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		atom, err := feed.ToAtom()
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+
+		output_path := fmt.Sprintf("%s/%s.atom", config.OutputPath, feed.Title)
+		out, err := os.Create(output_path)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		defer out.Close()
+		out.WriteString(atom)
+	}
+}
