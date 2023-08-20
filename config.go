9ea55be4afbbb2282a9477d9de6b37913cc815a7
Maxence <max@23.tf>
2023-08-20 13:40:37 -0400

    Extend config through env vars

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -40,6 +41,28 @@ func getFeedsFromConfig(path string) (*Config, error) {
 
 	setDefaults(&config)
 
+	extendConfigFromEnv(&config)
+
 	return &config, nil
 
+}
+
+func extendConfigFromEnv(config *Config) {
+	const PREFIX = "ATOMICBANQUET_"
+
+	for _, env := range os.Environ() {
+		keyval := strings.SplitN(env, "=", 2)
+		k, v := keyval[0], keyval[1]
+
+		if strings.HasPrefix(k, PREFIX) {
+			p := strings.Split(strings.TrimPrefix(k, PREFIX), "_")
+			feedKey := strings.ToLower(p[0])
+			optionKey := p[1]
+			for _, feed := range config.Feeds {
+				if feed.Module == feedKey {
+					feed.Options[optionKey] = v
+				}
+			}
+		}
+	}
 }
