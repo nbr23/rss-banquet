ab76b5fb6554e3217092dfa16793c0f7bf9787e6
Maxence <max@23.tf>
2024-06-11 07:03:06 -0400

    bring lego tests back

--- parser/lego/lego_test.go
+++ parser/lego/lego_test.go
@@ -1,39 +1,46 @@
 package lego
 
-// func TestLegoParseNew(t *testing.T) {
-// 	testsuite.TestParseSuccess(
-// 		t,
-// 		Lego{},
-// 		&parser.Options{
-// 			OptionsList: parser.OptionsList{
-// 				&parser.Option{
-// 					Flag:  "category",
-// 					Type:  "string",
-// 					Value: "new",
-// 				},
-// 			},
-// 			Parser: Lego{},
-// 		},
-// 		1,
-// 		`^\[\w+\] [0-9]+ - .* - (Available now|Pre-order this item today,|Will ship by) .*$`,
-// 	)
-// }
+import (
+	"testing"
 
-// func TestLegoParseComingSoon(t *testing.T) {
-// 	testsuite.TestParseSuccess(
-// 		t,
-// 		Lego{},
-// 		&parser.Options{
-// 			OptionsList: parser.OptionsList{
-// 				&parser.Option{
-// 					Flag:  "category",
-// 					Type:  "string",
-// 					Value: "coming-soon",
-// 				},
-// 			},
-// 			Parser: Lego{},
-// 		},
-// 		1,
-// 		`^\[\w+\] [0-9]+ - .* - Coming Soon .*$`,
-// 	)
-// }
+	"github.com/nbr23/rss-banquet/parser"
+	testsuite "github.com/nbr23/rss-banquet/utils"
+)
+
+func TestLegoParseNew(t *testing.T) {
+	testsuite.TestParseSuccess(
+		t,
+		Lego{},
+		&parser.Options{
+			OptionsList: parser.OptionsList{
+				&parser.Option{
+					Flag:  "category",
+					Type:  "string",
+					Value: "new",
+				},
+			},
+			Parser: Lego{},
+		},
+		1,
+		`^\[\w+\] [0-9]+ - .* - (Available now|Pre-order this item today,|Will ship by) .*$`,
+	)
+}
+
+func TestLegoParseComingSoon(t *testing.T) {
+	testsuite.TestParseSuccess(
+		t,
+		Lego{},
+		&parser.Options{
+			OptionsList: parser.OptionsList{
+				&parser.Option{
+					Flag:  "category",
+					Type:  "string",
+					Value: "coming-soon",
+				},
+			},
+			Parser: Lego{},
+		},
+		1,
+		`^\[\w+\] [0-9]+ - .* - Coming Soon .*$`,
+	)
+}
