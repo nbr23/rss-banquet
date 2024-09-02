a85a6647c75045360ee56e6c9d671eac89ead09c
Maxence <max@23.tf>
2024-09-01 22:04:44 -0400

    goodreads tests

---
+++ parser/goodreads/goodreads_test.go
@@ -0,0 +1,25 @@
+package goodreads
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/nbr23/rss-banquet/testsuite"
+)
+
+// Amélie Nothomb has been publishing yearly for 30 years. Don't break my tests!
+func TestAmelieNothomb(t *testing.T) {
+	options := GoodReads{}.GetOptions()
+	options.Set("authorId", "40416.Am_lie_Nothomb")
+	options.Set("language", "fr")
+	options.Set("year-min", fmt.Sprintf("%d", time.Now().Year()-1))
+	testsuite.TestParseSuccess(
+		t,
+		GoodReads{},
+		&options,
+		1,
+		`^.* - Amélie Nothomb$`,
+		`^Books by Amélie Nothomb - French$`,
+	)
+}
